04206f49b0a4157cd86f73d8aa313111b5b862d6
ericsudhartio <*ericsudhartio2509@gmail.com>
2019-10-31 19:32:56 +0700

    formLogin

---
+++ js/main.js
@@ -0,0 +1,94 @@
+$(document).ready(function () {
+  $('#signout').hide();
+  $('#login').show();
+  checkLogin()
+
+  $('#triggerSignin').submit(function (e) {
+    e.preventDefault();
+    signinM()
+  })
+})
+
+
+function signinM () {
+  const email = $('#email').val();
+  const password = $('#password').val();
+  $.ajax({
+    method: 'post',
+    url: 'http://localhost:3000/signin',
+    data: { email, password }
+  })
+    .then(data => {
+      //signin token sama usernya
+      Swal.fire({
+        position: 'top-end',
+        type: 'success',
+        title: 'success login',
+        timer: 500
+      })
+      localStorage.setItem('token', data.token)
+      $('#login').hide()
+      $('#signout').show()
+    })
+    .catch(err => {
+      Swal.fire({
+        title: 'Wooops Something Wrong Bro!',
+        animation: false,
+        customClass: {
+          popup: 'animated tada'
+        }
+      })
+    })
+}
+
+function checkLogin () {
+  if(localStorage.getItem('token')) {
+    $('#signout').show()
+    $('#login').hide()
+  } else {
+    $('#signout').hide()
+    $('#login').show()
+  }
+}
+
+
+function onSignIn(googleUser) {
+  var id_token = googleUser.getAuthResponse().id_token;
+  $.ajax({
+    method: 'post',
+    url: 'http://localhost:3000/signinG',
+    data: {
+      googleToken: id_token
+    }
+  })  
+    .then(data => {
+      localStorage.setItem('token', data.token)
+      Swal.fire({
+        position: 'top-start',
+        type: 'success',
+        title: 'You\'re logged in',
+        showConfirmButton: false,
+        timer: 700
+      })
+    })
+    .catch(err => {
+      Swal.fire({
+        title: 'Wooops Something Wrong Bro!',
+        animation: false,
+        customClass: {
+          popup: 'animated tada'
+        }
+      })
+    })
+}
+
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    localStorage.removeItem('token')
+    $('#signout').hide();
+    $('#login').show()
+    console.log('User signed out.');
+  });
+}
