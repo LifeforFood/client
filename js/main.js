7c8116755b6b25f637cd8132d3a494a37f2cf7d0
adityain98 <adityain98@gmail.com>
2019-10-31 21:47:41 +0700

    sementara

--- js/main.js
+++ js/main.js
@@ -48,13 +48,14 @@ function checkLogin () {
     $('#signout').show()
     $('#login').hide()
     $('#mainBody').show()
+    fetchData()
   } else {
     $('#signout').hide()
     $('#login').show()
     $('#mainBody').hide()
+    fetchData()
   }
 }
-
 
 function onSignIn(googleUser) {
   var id_token = googleUser.getAuthResponse().id_token;
@@ -89,13 +90,46 @@ function onSignIn(googleUser) {
     })
 }
 
-
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
     localStorage.removeItem('token')
     $('#signout').hide();
     $('#login').show()
+    $('#mainBody').hide()
     console.log('User signed out.');
   });
+}
+
+function fetchData(){
+  $.ajax({
+    url: 'http://localhost:3000/zomato/restaurants/74',
+    method: 'GET'
+  })
+    .done(restaurants=>{
+      restaurants.collections.forEach(restaurant=>{
+        const collection = restaurant.collection
+        console.log(collection);
+        $('#cards').append(`
+          <div class="col-md-4">
+            <div class="card mb-4 shadow-sm d-flex align">
+              <img class="bd-placeholder-img card-img-top" style="object-fit: contain;" src="${collection.image_url}" width="100%" height="225" >
+              <div class="card-body">
+                <p class="card-text" style="height: 100px" >${collection.description}</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Description</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Location</button>
+                  </div>
+                  <small class="text-muted">4/5 Star</small>
+                </div>
+              </div>
+            </div>
+          </div>
+        `)
+      })
+    })
+    .fail(err=>{
+      console.log(err)
+    })
 }
