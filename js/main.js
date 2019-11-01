a16dacfa53542e5a1b624b6f4a24e290026326e5
dadepe <rizky_dwifutra@yahoo.com>
2019-11-01 10:20:12 +0700

    baru lagi

--- js/main.js
+++ js/main.js
@@ -103,33 +103,86 @@ function signOut() {
 
 function fetchData(){
   $.ajax({
-    url: 'http://localhost:3000/zomato/restaurants/74',
-    method: 'GET'
+    method : 'GET',
+    url : `http://localhost:3000/zomato/restaurants/74/search?filter=pondok indah`
   })
-    .done(restaurants=>{
-      restaurants.collections.forEach(restaurant=>{
-        const collection = restaurant.collection
-        console.log(collection);
-        $('#cards').append(`
-          <div class="col-md-4">
-            <div class="card mb-4 shadow-sm d-flex align">
-              <img class="bd-placeholder-img card-img-top" style="object-fit: contain;" src="${collection.image_url}" width="100%" height="225" >
-              <div class="card-body">
-                <p class="card-text" style="height: 100px" >${collection.description}</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Description</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Location</button>
-                  </div>
-                  <small class="text-muted">4/5 Star</small>
+  .done(data=>{
+    $('#cards').empty()
+    console.log('MASUK SEARCH')
+
+    data.restaurants.forEach(restaurant=>{
+
+      $('#cards').append(`
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm d-flex align">
+            <img class="bd-placeholder-img card-img-top" style="object-fit: contain;" src="${restaurant.restaurant.featured_image}" width="100%" height="225" >
+            <div class="card-body">
+              <p class="card-text" style="height: 100px" >${restaurant.restaurant.name}</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Description</button>
+                  <button onclick="initMap(${restaurant.restaurant.location.latitude}, ${restaurant.restaurant.location.longitude})" type="button" class="btn btn-sm btn-outline-secondary">Location</button>
                 </div>
+                <small class="text-muted">4/5 Star</small>
               </div>
             </div>
           </div>
-        `)
+        </div>
+      `)
+    })
+  })
+  .fail(err=>{
+    console.log(err)
+  })
+}
+
+$('#formSearch').submit(function(e){
+  e.preventDefault()
+  $.ajax({
+    method : 'GET',
+    url : `http://localhost:3000/zomato/restaurants/74/search?filter=${$('#searchBox').val()}`
+  })
+  .done(data=>{
+    $('#cards').empty()
+    console.log('MASUK SEARCH')
+
+    data.restaurants.forEach(restaurant=>{
+      $('#cards').append(`
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm d-flex align">
+            <img class="bd-placeholder-img card-img-top" style="object-fit: contain;" src="${restaurant.restaurant.featured_image}" width="100%" height="225" >
+            <div class="card-body">
+              <p class="card-text" style="height: 100px" >${restaurant.restaurant.name}</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Description</button>
+                  <button onclick="initMap(${restaurant.restaurant.location.latitude}, ${restaurant.restaurant.location.longitude})" type="button" class="btn btn-sm btn-outline-secondary">Location</button>
+                </div>
+                <small class="text-muted">4/5 Star</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      `)
+    })
+  })
+  .fail(err=>{
+    console.log(err)
+  })
+})
+
+// MAP FUNCTION
+
+var map;
+function initMap(lat, lng) {
+  console.log('MASUK MAP')
+  $(`#myMap`).modal("show")
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat, lng},
+    zoom: 16
+  });
+  marker = new google.maps.Marker({
+          position: new google.maps.LatLng(lat, lng),
+          map: map
       })
-    })
-    .fail(err=>{
-      console.log(err)
-    })
 }
